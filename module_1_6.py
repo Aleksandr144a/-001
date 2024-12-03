7222950c7c728ca6970edfb40619ad8b1ae1326c
Aleksandr144a <tai144a@gmail.com>
2024-12-03 18:12:37 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+Quantit_comple_DZ=12
+Quantit_spent_time=1.5
+name_kurs='Pyton'
+time_1_task=1.5/12.0
+print('Курс:', name_kurs,'всего задач:',Quantit_comple_DZ, 'затрачено часов', Quantit_spent_time,
+ 'среднее время выполнения', time_1_task)
