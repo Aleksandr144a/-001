53901c1933c12ad586590f66da65c3580373a454
Aleksandr144a <tai144a@gmail.com>
2024-12-09 21:54:17 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string=input('Введите приветствие: ')
+print(my_string)
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print('первый символ: ', my_string[0])
+print('последний символ', my_string[-1])
